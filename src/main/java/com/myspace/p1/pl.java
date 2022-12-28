95060d97695073bef2614ae0a01e0acaad5203fd
wbadmin <>
2022-12-28 08:21:22 +0530

    {/src/main/java/com/myspace/p1/pl.java}

--- src/main/java/com/myspace/p1/pl.java
+++ src/main/java/com/myspace/p1/pl.java
@@ -6,28 +6,43 @@ package com.myspace.p1;
 
 public class pl implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public java.util.HashMap<String , Object> m;
-    
-    public pl() {
-    }
+	public java.util.HashMap<String, Object> m;
 
-public pl(java.util.HashMap<String , Object> m) {
-    this.m = m;
-    }
+	@org.kie.api.definition.type.Label(value = "a1")
+	private java.lang.Byte a1;
 
-    public void setM(java.util.HashMap<String , Object> m) {
-        this.m = m;
-    }
-    
-    public java.util.HashMap<String , Object> getM() {
-        return this.m;
-    }
-    
-    @Override
-    public String toString() {
-        return " [ m = " + m + " ] ";
-    }
+	public pl() {
+	}
+
+	public pl(java.util.HashMap<String, Object> m) {
+		this.m = m;
+	}
+
+	public void setM(java.util.HashMap<String, Object> m) {
+		this.m = m;
+	}
+
+	public java.util.HashMap<String, Object> getM() {
+		return this.m;
+	}
+
+	@Override
+	public String toString() {
+		return " [ m = " + m + " ] ";
+	}
+
+	public java.lang.Byte getA1() {
+		return this.a1;
+	}
+
+	public void setA1(java.lang.Byte a1) {
+		this.a1 = a1;
+	}
+
+	public pl(java.lang.Byte a1) {
+		this.a1 = a1;
+	}
 
 }
